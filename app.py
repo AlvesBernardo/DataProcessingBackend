8c2b530facc57eac40a045a2b86a6826a70431c5
Alves Bernardo <bernardo.correia.alves@student.nhlstenden.com>
2024-01-11 20:04:20 +0100

    Add: generate function

--- app.py
+++ app.py
@@ -3,18 +3,21 @@ from flask import Flask
 import sys
 sys.path.append("..") # added!
 from src.instances import quality_instances
-from services.auth_guard import auth_guard
+from services.auth_guard import check_jwt_token
+from services.jwt_handler import generate_jwt_token
 app = Flask(__name__)
 from flask import jsonify
-
-
+import requests
+from services.jwt_handler import decode_jwt_token
+from services.auth_guard import check_jwt_token
+from services.makeToken import make_token
 
 @app.route('/')
 def index():
     return render_template('index.html', subscriptions=quality_instances)
 
 @app.route('/select_subscription', methods=['POST'])
-@auth_guard()
+#@auth_guard()
 def select_subscription():
     selected_subscription_index = int(request.form['subscription'])
 
@@ -36,11 +39,24 @@ def get_subscription(subscription):
     else:
         return "User has not selected a subscription yet."
 
+#dummy generator function
+
+
 # Dummy protected route
 @app.route('/protected_route', methods=['GET'])
-@auth_guard()
 def protected_route():
-    return "This is a protected route. You can only access it with a valid token."
+    token = make_token()
 
+    try:
+        # Call check_jwt_token as a standalone function with the generated token
+        user_data = check_jwt_token(token)
+
+        # Now you have access to the decoded token information
+        user_id = user_data.get('user_id', None)
+
+        return jsonify({"message": "Token is valid!", "user_id": user_id})
+    
+    except Exception as e:
+        return jsonify({"message": f'Token validation failed: {e}', "status": 401}), 401
 if __name__ == '__main__':
     app.run(debug=True)
