3a135d38c0922db7107ed19362dd88a3332ad434
Mehdi Sadeghi <mehdi.sadeghi@student.nhlstenden.com>
2024-01-16 15:16:09 +0100

    Add email sending service and update environment variables
    An email sending service has been implemented with necessary environment variables included. The environment variables previously stored in app/config/.env are moved to .env directly under the root directory. In addition, emailSender-specific environment variables are added in .env. A configuration file (.idea/vcs.xml) to map the project directory to Git is also newly created.

--- app/app.py
+++ app/app.py
@@ -1,11 +1,12 @@
 from flask import Flask
+from app.services.emailSender import send_email
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return 'Hello World!'
+    send_email('mahdisadeghi.business@gmail.com')
 
 
 if __name__ == '__main__':
